3e9392b398929948eefcf64260e1463eebbe083e
vinceops <vince.g.135@gmail.com>
2023-06-01 10:02:13 +0200

    chore: upgrade packages to all latest versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,19 @@
+/** @type {import('eslint').ESLint.Options} */
 module.exports = {
-  extends: ['@vinceops/eslint-config/typescript'],
+  extends: [
+    "@vinceops/eslint-config/typescript"
+  ],
   plugins: ['jest'],
   rules: {
     'jest/expect-expect': 'off',
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/no-floating-promises": "warn"
   },
+  ignorePatterns: ["**/*.d.ts"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: ["./tsconfig.json", "./cypress/tsconfig.json"],
+    tsconfigRootDir: __dirname,
+    sourceType: "module"
+  }
 };
